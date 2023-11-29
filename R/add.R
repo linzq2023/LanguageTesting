a3608f9af92ff29e62ebff74a3fc78d4d601ec4d
linzq2023 <1376966691@qq.com>
2023-11-29 21:19:19 +0800

    initial commit

---
+++ R/add.R
@@ -0,0 +1,15 @@
+
+#' Find the sum of two numbers.
+#'
+#' @param x this is the first number
+#' @param y this is the second number
+#' @export
+#' @examples
+#' add(2,6)
+#' add (3,2)
+add<-function(x, y){
+  z = x +y
+  return(z)
+}
+
+
---
+++ R/minus1.R
@@ -0,0 +1,17 @@
+#' Minus one number to
+#'
+#' @param x the input number
+#' @return z
+#' @export
+#'
+#' @examples
+#' minus1(2)
+minus1<-function(x){
+  x = x-1
+  x
+}
+
+
+
+
+
