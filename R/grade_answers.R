b8333c878e7f85389db9f6ca764ac29cca40c408
linzq2023 <1376966691@qq.com>
2023-12-03 19:50:17 +0800

    student's response

---
+++ R/grade_answers.R
@@ -0,0 +1,43 @@
+#
+#' Dichotomize student's response
+#' @param df input the students' response
+#' @param correct_answers
+#'
+#' @return df dichotomized responses
+#' @export
+#'
+#' @examples
+#' library(readxl)
+#' students_df <- read_excel("students.xlsx")
+#' correct_answers <- c("A", "B", "C", "D")
+#' graded_df <- grade_answers(students_df, correct_answers)
+#' data<-data.frame(graded_df)
+#' data
+#'
+grade_answers <- function(df, correct_answers) {
+  # 遍历每个答案列
+  for (i in 1:length(correct_answers)) {
+    question_column <- paste("item", i, sep = "")
+    # 检查该列是否存在于数据框中
+    if (!question_column %in% names(df)) {
+      stop(paste("找不到列：", question_column))
+    }
+    # 根据正确答案进行评分
+    df[[question_column]] <- ifelse(df[[question_column]] == correct_answers[i], 1, 0)
+  }
+  return(df)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
